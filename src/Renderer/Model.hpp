d131cf8007aa4cc2c52d0abdb0d08d6ce45e2815
Starfunx <starfunx@hotmail.fr>
2021-12-26 12:18:33 +0100

    Index buffers

--- src/Renderer/Model.hpp
+++ src/Renderer/Model.hpp
@@ -30,9 +30,14 @@ public:
 
     };
     
-
-
-    Model(Device& device, const std::vector<Vertex> &vertices);
+    struct Builder
+    {
+        std::vector<Vertex> vertices{};
+        std::vector<uint32_t> indices{};
+    };
+    
+    
+    Model(Device& device, const Model::Builder &builder);
     ~Model();
 
     Model(const Model&) = delete;
@@ -44,13 +49,19 @@ public:
 
 private:
     void createVertexBuffers(const std::vector<Vertex> &vertices);
-
+    void createIndexBuffers(const std::vector<uint32_t> &indices);
+    
     /* data */
     Device& m_device;
 
     VkBuffer m_vertexBuffer;
     VkDeviceMemory m_vertexBufferMemory;
     uint32_t m_vertexCount;
+
+    bool m_hasIndexBuffer = false;
+    VkBuffer m_indexBuffer;
+    VkDeviceMemory m_indexBufferMemory;
+    uint32_t m_indexCount;
 };
 
 }
