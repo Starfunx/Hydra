cf94a5059a834e1b63eae2c74c5abdb4e7e7e327
Starfunx <starfunx@hotmail.fr>
2022-03-16 11:26:52 +0100

    trow error when unable to load image file

--- src/Ressources/Image.cpp
+++ src/Ressources/Image.cpp
@@ -3,12 +3,18 @@
 #define STB_IMAGE_IMPLEMENTATION   // use of stb functions once and for all
 #include "stb_image.h"
 
+// std
+#include <stdexcept>
+
 namespace hyd
 {
         
 Image::Image(const std::string& filepath)
 {
     m_imgData = stbi_load(filepath.c_str(), &m_width, &m_height, &m_channelsNumber, STBI_rgb_alpha);
+
+    if(m_imgData == nullptr)
+        throw(std::runtime_error("Failed to load Image: " + filepath));
 }
 
 Image::~Image()
